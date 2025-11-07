594dc21bc72499a8f71c549e7a6f668a2c471e21
ROXBOZ <roxboz@gmail.com>
2025-11-07 16:29:27 +0100

    Created Events Calendar

---
+++ public/modules/custom/roxboz_calendar/src/Controller/CalendarController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Drupal\roxboz_calendar\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Controller for the calendar page.
+ */
+class CalendarController extends ControllerBase {
+
+  /**
+   * Displays the weekly calendar view.
+   */
+  public function weekView() {
+    // Get current week dates.
+    $current_week = $this->getCurrentWeekDates();
+    
+    // Get events for the current week.
+    $events = $this->getWeekEvents($current_week['start'], $current_week['end']);
+    
+    return [
+      '#theme' => 'calendar_week_view',
+      '#week_dates' => $current_week,
+      '#events' => $events,
+      '#attached' => [
+        'library' => [
+          'roxboz_calendar/calendar-styling',
+        ],
+      ],
+    ];
+  }
+
+  /**
+   * Get dates for the current week (Monday to Friday).
+   */
+  private function getCurrentWeekDates() {
+    $today = new \DateTime();
+    
+    // Get Monday of current week.
+    $monday = clone $today;
+    $monday->modify('monday this week');
+    
+    $dates = [];
+    for ($i = 0; $i < 5; $i++) {
+      $date = clone $monday;
+      $date->modify("+$i days");
+      $dates[] = [
+        'date' => $date,
+        'day_name' => $date->format('l'),
+        'day_number' => $date->format('j'),
+        'month' => $date->format('M'),
+        'full_date' => $date->format('Y-m-d'),
+      ];
+    }
+    
+    return [
+      'start' => $monday->format('Y-m-d'),
+      'end' => $dates[4]['full_date'],
+      'dates' => $dates,
+    ];
+  }
+
+  /**
+   * Get events for the specified date range.
+   */
+  private function getWeekEvents($start_date, $end_date) {
+    // TODO: Query events from the database
+    // For now, return empty array
+    // We'll implement this after creating the Event content type
+    return [];
+  }
+
+}
--- public/modules/custom/roxboz_components/src/Plugin/Block/ButtonComponentBlock.php
+++ public/modules/custom/roxboz_components/src/Plugin/Block/ButtonComponentBlock.php
@@ -22,6 +22,8 @@ class ButtonComponentBlock extends BlockBase {
   public function defaultConfiguration() {
     return [
       'heading' => $this->t('Button Components Demo'),
+      'description' => $this->t('This is a description of the button components.'),
+      'image' => NULL,
     ];
   }
 
@@ -37,6 +39,27 @@ class ButtonComponentBlock extends BlockBase {
       '#required' => TRUE,
     ];
 
+    $form['description'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Description'),
+      '#description' => $this->t('Enter the description text for the button component.'),
+      '#default_value' => $this->configuration['description'],
+      '#required' => FALSE,
+    ];
+
+    $form['image'] = [
+      '#type' => 'managed_file',
+      '#title' => $this->t('Image'),
+      '#description' => $this->t('Upload an image for the button component.'),
+      '#default_value' => $this->configuration['image'] ? [$this->configuration['image']] : NULL,
+      '#upload_location' => 'public://button-component/',
+      '#upload_validators' => [
+        'file_validate_extensions' => ['png jpg jpeg gif svg'],
+        'file_validate_size' => [5242880], // 5MB
+      ],
+      '#required' => FALSE,
+    ];
+
     return $form;
   }
 
@@ -45,16 +68,50 @@ class ButtonComponentBlock extends BlockBase {
    */
   public function blockSubmit($form, FormStateInterface $form_state) {
     $this->configuration['heading'] = $form_state->getValue('heading');
+    $this->configuration['description'] = $form_state->getValue('description');
+    
+    // Handle image upload.
+    $image = $form_state->getValue('image');
+    if (!empty($image[0])) {
+      $this->configuration['image'] = $image[0];
+      // Mark file as permanent.
+      \Drupal::service('file.usage')->add(
+        \Drupal::entityTypeManager()->getStorage('file')->load($image[0]),
+        'roxboz_components',
+        'block',
+        1
+      );
+    }
+    else {
+      $this->configuration['image'] = NULL;
+    }
   }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
-    return [
+    $build = [
       '#theme' => 'button_component_block',
       '#heading' => $this->configuration['heading'],
+      '#description' => $this->configuration['description'],
+      '#image' => NULL,
     ];
+
+    // Render the image if available.
+    if (!empty($this->configuration['image'])) {
+      $file = \Drupal::entityTypeManager()->getStorage('file')->load($this->configuration['image']);
+      if ($file) {
+        $uri = $file->uri->value;
+        $build['#image'] = [
+          '#theme' => 'image',
+          '#uri' => \Drupal::service('file_url_generator')->generateAbsoluteString($uri),
+          '#alt' => $this->configuration['heading'],
+        ];
+      }
+    }
+
+    return $build;
   }
 
 }
